5a78da76ab6fdccc21b915b5a5d2d1e9c735805a
Hadi rasool <bigapplestorekmr@gmail.com>
2025-02-28 00:37:47 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Loading } from './components/Loading'
+import "./index.css"
+
+function App() {
+  return (
+    <div>
+      <Loading></Loading>
+    </div>
+  )
+}
+export default App
